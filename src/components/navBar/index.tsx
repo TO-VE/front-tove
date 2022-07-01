6a1b8f270670894a0791fe6ea38cdaa5e84b4981
JangAyeon <ayeon3052@ewhain.net>
2022-07-02 08:04:11 +0900

    check commit

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -4,6 +4,10 @@ const NavBar = () => {
   return (
     <StyledRoot>
       <Text>네비게이션</Text>
+      <span>오늘 기록</span>
+      <span>같이 사요</span>
+      <span>같이 해요</span>
+      <span>로그아웃</span>
     </StyledRoot>
   );
 };
