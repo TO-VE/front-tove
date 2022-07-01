4968c9f93ee941087b2ddb32bea0ff80a75ca127
JangAyeon <ayeon3052@ewhain.net>
2022-07-02 07:54:36 +0900

    Test : ThemeProvider Style Props

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import GlobalStyle from "styles/global";
 import { ThemeProvider } from "styled-components";
 import { theme } from "styles/theme";
 import { Task } from "types";
+import { NavBar } from "components";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme.light}>
         <GlobalStyle /> {/*스타일 리셋 & 구글 폰트 적용*/}
+        <NavBar />
         <div>최상위 컴포넌트</div>
       </ThemeProvider>
     </>
---
+++ src/components/navBar/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyledRoot, Text } from "./style";
+const NavBar = () => {
+  return (
+    <StyledRoot>
+      <Text>네비게이션</Text>
+    </StyledRoot>
+  );
+};
+
+export default NavBar;
