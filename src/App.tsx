10e85d77a0cac532da0a2d0f0ba9483bbf56e4c3
JangAyeon <ayeon3052@ewhain.net>
2022-07-02 07:32:02 +0900

    initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <ThemeProvider theme={theme.light}>
-        <GlobalStyle />
+        <GlobalStyle /> {/*스타일 리셋 & 구글 폰트 적용*/}
         <div>최상위 컴포넌트</div>
       </ThemeProvider>
     </>
